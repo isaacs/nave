61137bb61ef777478c8837ab58335a0123d77d6e
Luke Arduini <luke.arduini@gmail.com>
2013-03-15 19:35:35 -0400

    usemain copies local instead of downloading

--- nave.sh
+++ nave.sh
@@ -315,6 +315,13 @@ nave_usemain () {
     echo "$version already installed" >&2
     return 0
   fi
+  if nave_installed "$version"; then
+    echo "node v$version found locally, copying to global." >&2
+    cp $NAVE_ROOT/$version/bin/node $prefix/bin
+    cp $NAVE_ROOT/$version/bin/npm $prefix/bin
+    cp -r $NAVE_ROOT/$version/lib/node_modules/npm $prefix/lib/node_modules
+    return 0
+  fi
 
   build "$version" "$prefix"
 }
