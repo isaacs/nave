605d8f6908037b2118363744490385b0bf4bfd53
TayNelson <tay.nelson@yandex.com>
2015-09-29 21:09:40 +0100

    Fix binavail calculation for versions prior to 0.8.0

--- nave.sh
+++ nave.sh
@@ -251,7 +251,7 @@ build () {
     # binaries started with node 0.8.6
     case "$version" in
       0.8.[012345]) binavail=0 ;;
-      0.[1234567]) binavail=0 ;;
+      0.[1234567].*) binavail=0 ;;
       *) binavail=1 ;;
     esac
     if [ $binavail -eq 1 ]; then
