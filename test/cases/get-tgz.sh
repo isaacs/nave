44d3bada96cd1acc32270698396040a9349a7195
isaacs <i@izs.me>
2019-07-21 16:25:29 -0700

    get_tgz test

---
+++ test/cases/get-tgz.sh
@@ -0,0 +1,36 @@
+testdir=test/cases/get-tgz
+mkdir -p $testdir
+XDG_CONFIG_HOME=$(cd -- $testdir &>/dev/null ; pwd)
+. test/mocks/curl.sh
+_TESTING_NAVE_NO_MAIN=1 . nave.sh
+get_nave_dir
+# test that an invalid cache will be blown away and we'll still
+# get the correct shasum
+mkdir -p $testdir/nave/cache/v12.6.0
+touch $testdir/nave/cache/v12.6.0/SHASUMS256.txt
+
+# this echos the filename in the cache
+get_tgz v12.6.0/node-v12.6.0-darwin-x64.tar.gz
+
+# test that a shasum mismatch will not be a silent success
+mv $testdir/nave/cache/v12.6.0/SHASUMS256.txt{,.bak}
+cat > $testdir/nave/cache/v12.6.0/SHASUMS256.txt <<EOF
+___intentionally-invalid-shasum___  node-v12.6.0-darwin-x64.tar.gz
+EOF
+get_tgz v12.6.0/node-v12.6.0-darwin-x64.tar.gz
+mv $testdir/nave/cache/v12.6.0/SHASUMS256.txt{.bak,}
+
+# don't download if we don't have a shasum
+get_tgz v12.6.0/not-a-thing.tar.gz
+
+# fail if not cached and curl fails
+curl () {
+  return 1
+}
+rm test/cases/get-tgz/nave/cache/v12.6.0/*.tgz
+get_tgz v12.6.0/node-v12.6.0-darwin-x64.tar.gz
+echo $?
+
+find $testdir/nave
+
+rm -rf $testdir
