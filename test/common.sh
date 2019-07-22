f5517b0d2af9691c7952d3df0d1bdbfb2e1b8b54
isaacs <i@izs.me>
2019-07-21 23:12:39 -0700

    don't allow regular curl outside of snapshotting

--- test/common.sh
+++ test/common.sh
@@ -10,3 +10,12 @@ trap __nave_test_cleanup EXIT
 xdg () {
   export XDG_CONFIG_HOME=$(cd -- $testdir &>/dev/null ; pwd)
 }
+
+if [ "$SNAPSHOT" != "1" ]; then
+  curl () {
+    if [ "$1" != "--version" ]; then
+      echo "curl not allowed" >&2
+      exit 99
+    fi
+  }
+fi
