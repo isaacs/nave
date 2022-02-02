12e79bdfbce3b566e283e858f94aadbccbdfac3b
isaacs <i@izs.me>
2022-02-02 11:04:49 -0800

    xzx

--- test/cases/rcfile-silent-fail.sh
+++ test/cases/rcfile-silent-fail.sh
@@ -2,6 +2,7 @@
 mkdir -p $testdir/nave
 touch $testdir/nave/.zshenv
 chmod 0444 $testdir/nave/.zshenv
+ls -l $testdir/nave/.zshenv | awk '{print $1}'
 xdg
 _TESTING_NAVE_NO_HELP=1 _TESTING_NAVE_NO_EXIT=1 . nave.sh
 chmod 0644 $testdir/nave/.zshenv
