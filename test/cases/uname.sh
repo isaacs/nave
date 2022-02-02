8cd6ddbe558033f72d0e2c2538141df495dffc52
isaacs <i@izs.me>
2022-02-02 08:39:01 -0800

    tests for new uname additions

--- test/cases/uname.sh
+++ test/cases/uname.sh
@@ -3,6 +3,8 @@ unames=(
   "Darwin x86_64"
   "SunOS i486"
   "Linux i am a raspberrypie so delicious"
+  "Darwin moxy.lan 21.2.0 Darwin Kernel Version 21.2.0: Sun Nov 28 20:28:41 PST 2021; root:xnu-8019.61.5~1/RELEASE_ARM64_T6000 arm64"
+  "Linux aarch64"
 )
 for u in "${unames[@]}"; do
   uname () {
