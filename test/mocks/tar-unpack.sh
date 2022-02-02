e3b9badb2c17a73a04294d52c4998c2d9dd6f34f
isaacs <i@izs.me>
2022-02-02 11:41:28 -0800

    normalize tar

--- test/mocks/tar-unpack.sh
+++ test/mocks/tar-unpack.sh
@@ -3,13 +3,13 @@
 # care about the actual unpacked results.
 tar () {
   echo "unpacking mock: tar $@" >&2
+  cat >/dev/null
   local cmd=$1
   local filemaybe=$2
   shift
   if [[ $cmd =~ .*f ]]; then
     shift
   fi
-  $(which tar) xvf test/mocks/mock-node-tarball.tgz "$@" | \
-    sed -e '#^mock-node-v12.6.0/##g'
-  cat >/dev/null
+  $(which tar) xvf test/mocks/mock-node-tarball.tgz "$@" 2>&1 | \
+    sed -e '#^mock-node-v12.6.0/##g' >&2
 }
