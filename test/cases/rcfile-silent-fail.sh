b54b7599623a1bdb10627d9c2899bf4f7281f5cc
isaacs <i@izs.me>
2022-02-02 11:55:48 -0800

    macos

--- test/cases/rcfile-silent-fail.sh
+++ test/cases/rcfile-silent-fail.sh
@@ -4,5 +4,7 @@ touch $testdir/nave/.zshenv
 chmod 0444 $testdir/nave/.zshenv
 ls -l $testdir/nave/.zshenv | awk '{print $1}'
 xdg
+echo XDG_CONFIG_HOME=$XDG_CONFIG_HOME
 _TESTING_NAVE_NO_HELP=1 _TESTING_NAVE_NO_EXIT=1 . nave.sh
+cat $testdir/nave/.zshenv
 chmod 0644 $testdir/nave/.zshenv
