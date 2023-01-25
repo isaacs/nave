3e10d7c956a45e91ea9a91ab6f4756d541c1b3c5
isaacs <i@izs.me>
2023-01-25 09:18:22 -0800

    portability of nave-cache test case

--- test/cases/nave-cache.sh
+++ test/cases/nave-cache.sh
@@ -4,7 +4,9 @@ _TESTING_NAVE_NO_MAIN=1 . nave.sh
 nave_cache asdf
 touch $testdir/cache/foo
 mkdir -p $testdir/cache/a/{b,c}/d
-nave_cache tree
+# filter out the '<n> directories, 1 file' bit, since
+# that may or may not include root dir argument.
+nave_cache tree | grep -v 'directories, 1 file'
 nave_cache ls | sort
 nave_cache clear
 nave_cache ls
