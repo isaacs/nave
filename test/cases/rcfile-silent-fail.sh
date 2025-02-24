040b15045cfb1b6b829d151e51add1523c960cb4
isaacs <i@izs.me>
2025-02-24 12:26:53 -0800

    test: make rcfile test not have spurious failures

--- test/cases/rcfile-silent-fail.sh
+++ test/cases/rcfile-silent-fail.sh
@@ -2,7 +2,7 @@
 mkdir -p $testdir/nave
 touch $testdir/nave/.zshenv
 chmod 0444 $testdir/nave/.zshenv
-ls -l $testdir/nave/.zshenv | awk '{print $1}'
+ls -l $testdir/nave/.zshenv | awk '{print $1}' | grep -E -o '[rwx]'
 _TESTING_NAVE_NO_HELP=1 _TESTING_NAVE_NO_EXIT=1 . nave.sh
 cat $testdir/nave/.zshenv
 chmod 0644 $testdir/nave/.zshenv
